a537353b870978069aff40df39c9304efe612dc0
yash11213018 <52713940+yash11213018@users.noreply.github.com>
2020-09-14 21:48:27 +0530

    commit error page update// login page UI//

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
+      <Router>
+        <Switch>
+          {/*
+        path:the url.
+        exact: exactly the url must match.
+        */}
+
+          <Route path="/" exact={true}>
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          {/*
+        path:* refers that it will always match
+        */}
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
