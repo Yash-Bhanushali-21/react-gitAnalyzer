0f4f18b44dcff91fbaeb0bf735e0f73fa8f008d0
Yash Bhanushali <52713940+yash11213018@users.noreply.github.com>
2020-09-16 20:12:17 +0530

    followers section

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { MdBusiness, MdLocationOn, MdLink } from "react-icons/md";
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const {
+    avatar_url,
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          {/*
+          making sure if no twitter username,throw name.
+          */}
+          <p>@{twitter_username || { name }}</p>
+        </div>
+        <a href={html_url}>Follow</a>
+      </header>
+      <p className="bio">{bio}</p>
+      <div className="links">
+        <p>
+          <MdBusiness /> {company}
+        </p>
+        <p>
+          <MdLocationOn /> {location || "earth"}
+        </p>
+        <p>
+          <a href={"https://".concat(blog)}>
+            <MdLink /> {blog}
+          </a>
+        </p>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
@@ -13,7 +53,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: "user";
     position: absolute;
     top: 0;
     left: 0;
