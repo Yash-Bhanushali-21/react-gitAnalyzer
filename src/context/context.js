fa5c1a2547e1a3f1ae8eff0ad65c64a1e57e5b17
Yash Bhanushali <52713940+yash11213018@users.noreply.github.com>
2020-09-16 18:28:14 +0530

    info. js update. static data render

--- src/context/context.js
+++ src/context/context.js
@@ -4,6 +4,7 @@ import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
 import axios from "axios";
 
+//api we will be calling for user.
 const rootUrl = "https://api.github.com";
 
 /*
@@ -17,8 +18,17 @@ const GithubContext = React.createContext();
 //accessing via. gitContext.Provider rather than props.
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+
   return (
-    <GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>
+    //this means if we get
+    //req as githubuser:it wwill set github user.
+    //githubuser:this_scopes_githubuser
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
