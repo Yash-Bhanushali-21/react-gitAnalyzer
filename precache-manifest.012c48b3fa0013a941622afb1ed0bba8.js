b56c5046c1fba34ec13825d5dec64d8952c2d880
Yash Bhanushali <yash.bhanushali@ayraa.io>
2022-11-25 15:01:09 +0530

    Updates

---
+++ precache-manifest.012c48b3fa0013a941622afb1ed0bba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fafd942485296b5d01861e6e11639b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9268dcdf5ee35526d0a1",
+    "url": "/static/css/main.b854109b.chunk.css"
+  },
+  {
+    "revision": "027e03717d050be8748d",
+    "url": "/static/js/2.b57febe3.chunk.js"
+  },
+  {
+    "revision": "d50cbe733d7e1e83d34da7158551fa9c",
+    "url": "/static/js/2.b57febe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9268dcdf5ee35526d0a1",
+    "url": "/static/js/main.4bf304ab.chunk.js"
+  },
+  {
+    "revision": "864964386b8fbd0a16b5",
+    "url": "/static/js/runtime-main.0fe5df00.js"
+  },
+  {
+    "revision": "20a909842ebf13585654be5bada6a463",
+    "url": "/static/media/login-img.20a90984.svg"
+  }
+]);
